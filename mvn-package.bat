b2886ec65b720f514822cd7788faef9f058857fb
George Kolk <pala2525@gmail.com>
2019-11-14 14:11:11 +0200

    build fix

--- mvn-package.bat
+++ mvn-package.bat
@@ -1,3 +1,4 @@
 mkdir src\main\java\org\metacow\InPowerWeEntrustLoveAdvocated
-copy src\*.* \main\java\org\metacow\InPowerWeEntrustLoveAdvocated
+copy src\*.* src\main\java\org\metacow\InPowerWeEntrustLoveAdvocated
 mvn package
+RMDIR /S /Q src\main
