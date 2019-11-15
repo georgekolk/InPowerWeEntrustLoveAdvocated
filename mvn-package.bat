6a5a25caa7fdafb11d4a1d4e7999f0f8ce078338
George Kolk <pala2525@gmail.com>
2019-11-15 15:26:24 +0200

    command line is wurking

--- mvn-package.bat
+++ mvn-package.bat
@@ -1,6 +1,6 @@
 mkdir src\main\java\org\metacow\InPowerWeEntrustLoveAdvocated
 copy src\*.* src\main\java\org\metacow\InPowerWeEntrustLoveAdvocated
 mvn package
-RMDIR /S /Q src\main
-copy InPowerWeEntrustLoveAdvocated\target\InPowerWeEntrustLoveAdvocated-1.0-SNAPSHOT-jar-with-dependencies.jar c:\prod
+rem RMDIR /S /Q src\main
+rem copy InPowerWeEntrustLoveAdvocated\target\InPowerWeEntrustLoveAdvocated-1.0-SNAPSHOT-jar-with-dependencies.jar c:\prod
 rem copy InPowerWeEntrustLoveAdvocated\config.json c:\prod
